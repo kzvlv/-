4ba19bde90a65b25a790ef4127325f155580c679
Кирилл <Kirill.kuzovlev2006@gmail.com>
2025-08-22 12:49:02 +0800

    start_test

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world!")
+import subprocess
+import sys
+
+# Запуск другого Python-файла
+get_files_to_person = subprocess.run([sys.executable, "get_files_to_person.py"],
+                       capture_output=True, text=True)
+
+if get_files_to_person.stderr:
+    print("Ошибки:", get_files_to_person.stderr)
+
+# Запуск другого Python-файла
+get_info_about_person = subprocess.run([sys.executable, "get_info_about_person.py"],
+                       capture_output=True, text=True)
+
+if get_info_about_person.stderr:
+    print("Ошибки:", get_info_about_person.stderr)
