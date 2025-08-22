085473da3f4fe989d0d3181af20a18d2511ade23
Кирилл <Kirill.kuzovlev2006@gmail.com>
2025-08-22 12:49:53 +0800

    start_test

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import subprocess
 import sys
 
-# Запуск другого Python-файла
+# Запуск Python-файла
 get_files_to_person = subprocess.run([sys.executable, "get_files_to_person.py"],
                        capture_output=True, text=True)
 
 if get_files_to_person.stderr:
     print("Ошибки:", get_files_to_person.stderr)
 
-# Запуск другого Python-файла
+# Запуск Python-файла
 get_info_about_person = subprocess.run([sys.executable, "get_info_about_person.py"],
                        capture_output=True, text=True)
 
